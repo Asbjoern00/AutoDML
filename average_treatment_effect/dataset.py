de504ca02bac562f9aba358fef485ceaf28e158f
AsgerWP <lzt134@alumni.ku.dk>
2025-11-19 13:52:42 +0100

    tests: Dataset has correct attributes

--- average_treatment_effect/dataset.py
+++ average_treatment_effect/dataset.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
 import numpy as np
+from typing import Optional
 
 
 @dataclass
 class Dataset:
-    treatments: np.ndarray
-    outcomes: np.ndarray
-    counterfactual_outcomes: np.ndarray
-    noiseless_untreated_outcomes: np.ndarray
-    noiseless_treated_outcomes: np.ndarray
-    covariates: np.ndarray
+    treatments: Optional[np.ndarray] = None
+    outcomes: Optional[np.ndarray] = None
+    counterfactual_outcomes: Optional[np.ndarray] = None
+    noiseless_untreated_outcomes: Optional[np.ndarray] = None
+    noiseless_treated_outcomes: Optional[np.ndarray] = None
+    covariates: Optional[np.ndarray] = None
 
     def get_average_treatment_effect(self):
         return np.mean(
--- average_treatment_effect/tests/test_dataset.py
+++ average_treatment_effect/tests/test_dataset.py
@@ -1,15 +1,26 @@
 import numpy as np
 from average_treatment_effect.dataset import Dataset
+from dataclasses import fields
 
 
 class TestDataset:
+    def test_has_correct_attributes(self):
+        dataset = Dataset()
+        _fields = fields(dataset)
+        expected_fields = [
+            "treatments",
+            "outcomes",
+            "counterfactual_outcomes",
+            "noiseless_untreated_outcomes",
+            "noiseless_treated_outcomes",
+            "covariates",
+        ]
+        assert len(_fields) == len(expected_fields)
+        assert [field.name for field in _fields] == expected_fields
+
     def test_can_calculate_empirical_average_treatment_effect(self):
         dataset = Dataset(
-            treatments=np.zeros((5, 1)),
-            outcomes=np.zeros((5, 1)),
-            counterfactual_outcomes=np.zeros((5, 1)),
             noiseless_untreated_outcomes=np.array([1, 0, 1, 4, 5]),
             noiseless_treated_outcomes=np.array([2, 1, 2, 5, 6]),
-            covariates=np.zeros((5, 5)),
         )
         assert dataset.get_average_treatment_effect() == 1
