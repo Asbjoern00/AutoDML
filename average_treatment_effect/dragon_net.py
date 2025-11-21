b6a3b9bdddc63debaae1997be86bf53be98c9539
AsgerWP <lzt134@alumni.ku.dk>
2025-11-21 12:27:06 +0100

    feat: create OutcomeNet

--- average_treatment_effect/dragon_net.py
+++ average_treatment_effect/dragon_net.py
@@ -3,6 +3,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class OutcomeNet(nn.Module):
+    def __init__(self):
+        super(OutcomeNet, self).__init__()
+        self.layer1 = nn.Linear(200, 100)
+        self.layer2 = nn.Linear(100, 100)
+        self.layer3 = nn.Linear(100, 1)
+
+    def forward(self, shared_state):
+        x = self.layer1(shared_state)
+        x = F.elu(x)
+        x = self.layer2(x)
+        x = F.elu(x)
+        x = self.layer3(x)
+        return x
+
+
 class SharedNet(nn.Module):
     def __init__(self):
         super(SharedNet, self).__init__()
--- average_treatment_effect/tests/test_dragon_net.py
+++ average_treatment_effect/tests/test_dragon_net.py
@@ -7,3 +7,9 @@ class TestDragonNet:
         number_of_parameters = sum(p.numel() for p in model.parameters())
         expected_number_of_parameters = 25 * 200 + 200 + 200 * 200 + 200 + 200 * 200 + 200
         assert number_of_parameters == expected_number_of_parameters
+
+    def test_outcome_net_has_correct_number_of_parameters(self):
+        model = dragon_net.OutcomeNet()
+        number_of_parameters = sum(p.numel() for p in model.parameters())
+        expected_number_of_parameters = 200 * 100 + 100 + 100 * 100 + 100 + 100 * 1 + 1
+        assert number_of_parameters == expected_number_of_parameters
