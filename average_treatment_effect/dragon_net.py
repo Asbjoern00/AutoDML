a4f61432a80536b17fc58faf901f5763d241cabe
AsgerWP <lzt134@alumni.ku.dk>
2025-11-21 12:34:28 +0100

    feat: create BaseDragonNet

--- average_treatment_effect/dragon_net.py
+++ average_treatment_effect/dragon_net.py
@@ -3,6 +3,30 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class BaseDragonNet(nn.Module):
+    def __init__(self):
+        super(BaseDragonNet, self).__init__()
+        self.shared_net = SharedNet()
+        self.treated_outcome_net = OutcomeNet()
+        self.untreated_outcome_net = OutcomeNet()
+        self.propensity_output_layer = nn.Linear(200, 1)
+
+    def forward(self, covariates, treatment):
+        shared_state = self.shared_net(covariates)
+        propensity_logit = self.propensity_output_layer(shared_state)
+        treatment_prediction = torch.sigmoid(propensity_logit)
+        untreated_outcome_prediction = self.untreated_outcome_net(shared_state)
+        treated_outcome_prediction = self.treated_outcome_net(shared_state)
+        outcome_prediction = treatment * treated_outcome_prediction + (1 - treatment) * untreated_outcome_prediction
+
+        return {
+            "treatment_prediction": treatment_prediction,
+            "untreated_outcome_prediction": untreated_outcome_prediction,
+            "treated_outcome_prediction": treated_outcome_prediction,
+            "outcome_prediction": outcome_prediction,
+        }
+
+
 class OutcomeNet(nn.Module):
     def __init__(self):
         super(OutcomeNet, self).__init__()
--- average_treatment_effect/tests/test_dragon_net.py
+++ average_treatment_effect/tests/test_dragon_net.py
@@ -5,11 +5,17 @@ class TestDragonNet:
     def test_shared_net_has_correct_number_of_parameters(self):
         model = dragon_net.SharedNet()
         number_of_parameters = sum(p.numel() for p in model.parameters())
-        expected_number_of_parameters = 25 * 200 + 200 + 200 * 200 + 200 + 200 * 200 + 200
+        expected_number_of_parameters = 85600
         assert number_of_parameters == expected_number_of_parameters
 
     def test_outcome_net_has_correct_number_of_parameters(self):
         model = dragon_net.OutcomeNet()
         number_of_parameters = sum(p.numel() for p in model.parameters())
-        expected_number_of_parameters = 200 * 100 + 100 + 100 * 100 + 100 + 100 * 1 + 1
+        expected_number_of_parameters = 30301
+        assert number_of_parameters == expected_number_of_parameters
+
+    def test_base_dragon_net_has_correct_number_of_parameters(self):
+        model = dragon_net.BaseDragonNet()
+        number_of_parameters = sum(p.numel() for p in model.parameters())
+        expected_number_of_parameters = 2 * 30301 + 85600 + 200 + 1
         assert number_of_parameters == expected_number_of_parameters
