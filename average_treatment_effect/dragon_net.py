e4c35f3a212c4da3d926957de90d5d646f5d8372
AsgerWP <lzt134@alumni.ku.dk>
2025-11-21 17:39:11 +0100

    refactor: inline sharednet

--- average_treatment_effect/dragon_net.py
+++ average_treatment_effect/dragon_net.py
@@ -10,21 +10,20 @@ class DragonNet(nn.Module):
         self.epsilon = nn.Parameter(torch.zeros(1))
 
     def forward(self, covariates, treatments):
-        base_output = self.base_dragonNet(covariates, treatments)
-        untreated_outcome_prediction = base_output["untreated_outcome_predictions"] + self.epsilon / (
-            1 - base_output["treatment_predictions"]
-        )
-        treated_outcome_prediction = (
-            base_output["treated_outcome_predictions"] + self.epsilon / base_output["treatment_predictions"]
-        )
-        targeted_outcome_prediction = (
-            treatments * treated_outcome_prediction + (1 - treatments) * untreated_outcome_prediction
-        )
+        output = self.base_dragonNet(covariates, treatments)
+        treatment_predictions = output["treatment_predictions"]
+        q0 = output["untreated_outcome_predictions"]
+        q1 = output["treated_outcome_predictions"]
+
+        q0 = q0 + self.epsilon / (1 - treatment_predictions)
+        q1 = q1 + self.epsilon / treatment_predictions
+        targeted_outcome_prediction = treatments * q1 + (1 - treatments) * q0
+
         return {
-            "treatment_predictions": base_output["treatment_predictions"],
-            "untreated_outcome_predictions": untreated_outcome_prediction,
-            "treated_outcome_predictions": treated_outcome_prediction,
-            "outcome_predictions": base_output["outcome_predictions"],
+            "treatment_predictions": treatment_predictions,
+            "untreated_outcome_predictions": q0,
+            "treated_outcome_predictions": q1,
+            "outcome_predictions": output["outcome_predictions"],
             "targeted_outcome_predictions": targeted_outcome_prediction,
         }
 
@@ -32,15 +31,23 @@ class DragonNet(nn.Module):
 class BaseDragonNet(nn.Module):
     def __init__(self):
         super(BaseDragonNet, self).__init__()
-        self.shared_net = SharedNet()
+        self.shared_layer1 = nn.Linear(25, 200)
+        self.shared_layer2 = nn.Linear(200, 200)
+        self.shared_layer3 = nn.Linear(200, 200)
+
         self.treated_outcome_net = OutcomeNet()
         self.untreated_outcome_net = OutcomeNet()
-        self.propensity_output_layer = nn.Linear(200, 1)
+        self.treatment_prediction_layer = nn.Linear(200, 1)
 
     def forward(self, covariates, treatments):
-        shared_state = self.shared_net(covariates)
-        propensity_logit = self.propensity_output_layer(shared_state)
-        treatment_prediction = torch.sigmoid(propensity_logit).clamp(1e-3, 1 - 1e-3)
+        shared_state = self.shared_layer1(covariates)
+        shared_state = F.elu(shared_state)
+        shared_state = self.shared_layer2(shared_state)
+        shared_state = F.elu(shared_state)
+        shared_state = self.shared_layer3(shared_state)
+        shared_state = F.elu(shared_state)
+
+        treatment_prediction = self.treatment_prediction_layer(shared_state).sigmoid().clamp(1e-3, 1 - 1e-3)
         untreated_outcome_prediction = self.untreated_outcome_net(shared_state)
         treated_outcome_prediction = self.treated_outcome_net(shared_state)
         outcome_prediction = treatments * treated_outcome_prediction + (1 - treatments) * untreated_outcome_prediction
@@ -66,24 +73,6 @@ class OutcomeNet(nn.Module):
         x = self.layer2(x)
         x = F.elu(x)
         x = self.layer3(x)
-        return x
-
-
-class SharedNet(nn.Module):
-    def __init__(self):
-        super(SharedNet, self).__init__()
-        self.layer1 = nn.Linear(25, 200)
-        self.layer2 = nn.Linear(200, 200)
-        self.layer3 = nn.Linear(200, 200)
-
-    def forward(self, covariates):
-        x = self.layer1(covariates)
-        x = F.elu(x)
-        x = self.layer2(x)
-        x = F.elu(x)
-        x = self.layer3(x)
-        x = F.elu(x)
-
         return x
 
 
