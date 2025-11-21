73c6490b7efaf3b359d69ec0c3b75dd5e58cb488
AsgerWP <lzt134@alumni.ku.dk>
2025-11-21 23:49:11 +0100

    fix: forward method

--- average_treatment_effect/dragon_net/dragon_net.py
+++ average_treatment_effect/dragon_net/dragon_net.py
@@ -16,12 +16,12 @@ class DragonNet(nn.Module):
 
     def forward(self, covariates, treatments):
         shared_state = self.shared_layers(covariates)
-        treatment_prediction = self.treatment_prediction_layer(treatments)
+        treatment_prediction = self.treatment_prediction_layer(shared_state).clamp(1e-3, 1 - 1e-3)
         q0 = self.q0_layers(shared_state)
-        q1 = self.q1_layers(treatment_prediction)
+        q1 = self.q1_layers(shared_state)
         base_outcome_prediction = (1 - treatments) * q0 + treatments * q1
-        q0 = q0 + self.epsilon / treatment_prediction
-        q1 = q1 + self.epsilon / (1 - treatment_prediction)
+        q0 = q0 + self.epsilon / (1 - treatment_prediction)
+        q1 = q1 + self.epsilon / treatment_prediction
         targeted_outcome_prediction = (1 - treatments) * q0 + treatments * q1
         return {
             "treatment_prediction": treatment_prediction,
