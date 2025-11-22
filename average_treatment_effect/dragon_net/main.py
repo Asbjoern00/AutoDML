6b680c6e628aeb6048266658bd43f189541757e6
AsgerWP <lzt134@alumni.ku.dk>
2025-11-22 18:24:20 +0100

    feat: add file for running experiment

---
+++ average_treatment_effect/dragon_net/main.py
@@ -0,0 +1,20 @@
+import torch
+from average_treatment_effect import dataset
+from average_treatment_effect.dragon_net.dragon_net_wrapper import DragonNetWrapper
+
+torch.manual_seed(42)
+estimates = []
+truths = []
+for i in range(1000):
+    print("Iteration", i + 1)
+    data = dataset.Dataset.load_chernozhukov_replication(i + 1)
+    truth = data.get_average_treatment_effect()
+    truths.append(truth)
+    dragon_net = DragonNetWrapper.create()
+    dragon_net.train(data)
+    estimate = dragon_net.get_average_treatment_effect(data)
+    estimates.append(estimate)
+    MAE = sum(abs(est - truth) for est, truth in zip(estimates, truths)) / len(estimates)
+    print("estimate", estimate)
+    print("Truth:", truth)
+    print("MAE:", MAE)
