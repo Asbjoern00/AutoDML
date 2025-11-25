676c85419bf80952f2a4c585425973be12b4ad56
asbjoern00 <asbjoern00@gmail.com>
2025-11-25 16:58:03 +0100

    test average derivative functional

--- AveragePartialDerivative/AveragePartialDerivativeFunctional.py
+++ AveragePartialDerivative/AveragePartialDerivativeFunctional.py
@@ -4,9 +4,12 @@ def avg_der_functional(data, evaluator):
     T = torch.autograd.Variable(data[:, [0]], requires_grad=True)
     input = torch.cat([T, data[:, 1:]], dim=1)
     output = evaluator(input)
-    gradients = torch.autograd.grad(outputs=output, inputs=T,
-                          grad_outputs=torch.ones(output.size()),
-                          create_graph=True, retain_graph=True, only_inputs=True)[0]
-
+    gradients = torch.autograd.grad(
+        outputs=output,
+        inputs=T,
+        grad_outputs=torch.ones(output.size()),
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True
+    )[0]
     return gradients
-
