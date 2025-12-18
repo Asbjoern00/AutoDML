25d58d98040e76490e40cc2c52fd4c2b88a61197
AsgerWP <lzt134@alumni.ku.dk>
2025-12-18 14:24:42 +0100

    feat: add regression functions

--- ate_experiment/dataset.py
+++ ate_experiment/dataset.py
@@ -9,6 +9,17 @@ class Dataset:
         covariate_columns = [i for i in range(raw_data.shape[1]) if i not in [outcome_column, treatment_column]]
         self.covariate_columns = covariate_columns
 
+    @staticmethod
+    def outcome_regression(covariates, treatments):
+        treated_regression = covariates[:, 0] ** 2 + covariates[:, 2] + covariates[:, 3]
+        control_regression = covariates[:, 0] ** 2 + covariates[:, 2] + covariates[:, 3]
+        return treatments * treated_regression + (1 - treatments) * control_regression
+
+    @staticmethod
+    def propensity_score(covariates):
+        logit = covariates[:, 0] ** 2 + covariates[:, 2] + covariates[:, 3]
+        return 1 / (1 + np.exp(-logit))
+
     @property
     def outcome(self):
         return self.raw_data[:, self.outcome_column]
