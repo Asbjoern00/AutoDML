8c2eb8f12bec3694729d2e49f9f684f0620db6db
AsgerWP <lzt134@alumni.ku.dk>
2025-12-18 14:20:50 +0100

    feat: property methods

--- ate_experiment/dataset.py
+++ ate_experiment/dataset.py
@@ -6,4 +6,17 @@ class Dataset:
         self.raw_data = raw_data
         self.outcome_column = outcome_column
         self.treatment_column = treatment_column
-        self.covariate_columns = [i for i in range(raw_data.shape[1]) if i not in [outcome_column, treatment_column]]
+        covariate_columns = [i for i in range(raw_data.shape[1]) if i not in [outcome_column, treatment_column]]
+        self.covariate_columns = covariate_columns
+
+    @property
+    def outcome(self):
+        return self.raw_data[:, self.outcome_column]
+
+    @property
+    def treatment(self):
+        return self.raw_data[:, self.treatment_column]
+
+    @property
+    def covariates(self):
+        return self.raw_data[:, self.covariate_columns]
