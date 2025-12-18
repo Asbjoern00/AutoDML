16611702e8228b3b7c95fc9785eb96e1f25a3b2c
AsgerWP <lzt134@alumni.ku.dk>
2025-12-18 17:44:52 +0100

    make dataset unbalanced

--- ate_experiment/gradient_boosting_experiment/main.py
+++ ate_experiment/gradient_boosting_experiment/main.py
@@ -8,30 +8,43 @@ plug_ins = []
 propensity_ests = []
 riesz_ests = []
 
-iterations = 100
+iterations = 1000
 number_of_samples = 1000
+lambda_ = 3
+
 
 for i in range(iterations):
 
     print(i)
 
-    data = Dataset.simulate_dataset(number_of_samples=number_of_samples, number_of_covariates=20)
-    print(sum(data.treatments))
-    outcome_params = {"objective": "reg:squarederror", "eval_metric": "rmse", "max_depth": 3, "eta": 0.1}
+    data = Dataset.simulate_dataset(number_of_samples=number_of_samples, number_of_covariates=1)
+    outcome_params = {
+        "objective": "reg:squarederror",
+        "eval_metric": "rmse",
+        "max_depth": 3,
+        "eta": 0.1,
+        "lambda": lambda_,
+    }
     outcome_model = OutcomeXGBModel(outcome_params)
     outcome_model.fit(data)
     outcome_predictions = outcome_model.get_predictions(data)
     plug_in = np.mean(outcome_predictions["treated_predictions"] - outcome_predictions["control_predictions"])
 
-    propensity_params = {"objective": "binary:logistic", "eval_metric": "logloss", "max_depth": 3, "eta": 0.1}
+    propensity_params = {
+        "objective": "binary:logistic",
+        "eval_metric": "logloss",
+        "max_depth": 3,
+        "eta": 0.1,
+        "lambda": lambda_,
+    }
     propensity_model = PropensityXGBModel(propensity_params)
     propensity_model.fit(data)
     propensity_riesz_representer = propensity_model.get_riesz_representer(data)
     propensity_correction = np.mean(propensity_riesz_representer * (data.outcomes - outcome_predictions["predictions"]))
     propensity_estimate = plug_in + propensity_correction
 
-    riesz_params = {"disable_default_eval_metric": True, "max_depth": 3, "eta": 0.1}
-    riesz_model = RieszXGBModel(riesz_params, hessian_correction=1/number_of_samples)
+    riesz_params = {"disable_default_eval_metric": True, "max_depth": 3, "eta": 0.1, "lambda": lambda_}
+    riesz_model = RieszXGBModel(riesz_params, hessian_correction=0)
     riesz_model.fit(data)
     riesz_riesz_representer = riesz_model.get_riesz_representer(data)
     riesz_correction = np.mean(riesz_riesz_representer * (data.outcomes - outcome_predictions["predictions"]))
