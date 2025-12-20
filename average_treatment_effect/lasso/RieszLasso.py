2dcf25240c21d4a184f7fcaf4c9f1eeb6dea2665
asbjoern00 <asbjoern00@gmail.com>
2025-12-20 11:19:48 +0100

    feat: PropensityLasso

--- average_treatment_effect/lasso/RieszLasso.py
+++ average_treatment_effect/lasso/RieszLasso.py
@@ -1,11 +1,10 @@
 import numpy as np
 from average_treatment_effect.lasso.md_lasso import md_lasso
-
+from sklearn.linear_model import LogisticRegressionCV
 
 class RieszLasso:
-    def __init__(self, functional, penalty = 0.05):
+    def __init__(self, functional):
         self.functional = functional
-        self.penalty = penalty
         self.rho = None
 
     @staticmethod
@@ -19,14 +18,31 @@ class RieszLasso:
 
         return design
 
-    def fit(self, data):
+    def fit(self, data, penalty = 0.05):
         xb = self.make_design_matrix(data)
         mb = self.functional(data, self.make_design_matrix)
         hatM = np.mean(mb, axis=0)
         hatG = 1 / xb.shape[0] * (xb.T @ xb)
-        rho = md_lasso(hatG, hatM, penalty=self.penalty)
+        #If rho is already fitted, warm state from rho
+        rho = md_lasso(hatG, hatM, penalty=penalty, rho_init=self.rho)
         self.rho = rho
 
     def get_riesz_representer(self, data):
         xb = self.make_design_matrix(data)
         return xb @ self.rho
+
+
+class PropensityLasso:
+    def __init__(self):
+        self.model = LogisticRegressionCV(penalty="l1", fit_intercept=True, solver = "liblinear")
+
+    def fit(self, data):
+        covariates = data.covariates
+        treatments = data.treatments
+        self.model.fit(covariates, treatments)
+
+    def get_riesz_representer(self, data):
+        covariates = data.covariates
+        treatments = data.treatments
+        pi = self.model.predict_proba(covariates)[:,self.model.classes_ == 1]
+        return treatments/pi - (1-treatments)/(1-pi)
