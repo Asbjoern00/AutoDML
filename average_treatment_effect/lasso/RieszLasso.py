bea852e8bf99403446f8c392924268ea2b3c632a
asbjoern00 <asbjoern00@gmail.com>
2025-12-20 10:29:24 +0100

    feat : RieszLasso

---
+++ average_treatment_effect/lasso/RieszLasso.py
@@ -0,0 +1,25 @@
+import numpy as np
+from average_treatment_effect.lasso.md_lasso import md_lasso
+
+
+class RieszLasso:
+    def __init__(self, functional):
+        self.functional = functional
+
+    @staticmethod
+    def make_design_matrix(data, include_intercept=True):
+        if include_intercept:
+            design = np.concatenate(
+            [np.ones(data.treatments.shape[0]).reshape(-1,1), data.treatments.reshape(-1, 1), data.covariates], axis=1
+        )
+        else:
+            design = np.concatenate([data.treatments.reshape(-1, 1), data.covariates], axis=1)
+
+        return design
+
+    def fit(self, data):
+        xb = self.make_design_matrix(data)
+        mb = self.functional(data, self.make_design_matrix)
+        hatM = np.mean(mb, axis=0)
+        hatG = 1 / xb.shape[0] * (xb.T @ xb)
+        rho = md_lasso(hatG, hatM, 0.01)
---
+++ average_treatment_effect/lasso/md_lasso.py
@@ -0,0 +1,51 @@
+import numpy as np
+from numba import njit
+
+@njit
+def md_lasso(G, M, rL, D=None, rho_init=None, max_iter=5000, tol=1e-3):
+    p = M.shape[0]
+
+    if D is None:
+        D = np.ones(p)
+    D = D.copy()
+    D[0] = 0.0  # intercept is not penalized
+
+    if rho_init is None:
+        rho = np.zeros(p)
+    else:
+        rho = rho_init.copy()
+
+    z = np.diag(G)
+    Grho = G @ rho  # cache G rho
+
+    for it in range(max_iter):
+        max_change = 0.0
+
+        for j in range(p):
+            # π_j = M_j − (Gρ)_j + G_jj ρ_j
+            pi_j = M[j] - Grho[j] + z[j] * rho[j]
+
+            if j == 0:
+                # intercept: no penalty
+                new_rj = pi_j / z[j]
+            else:
+                thresh = D[j] * rL
+
+                if pi_j < -thresh:
+                    new_rj = (pi_j + thresh) / z[j]
+                elif pi_j > thresh:
+                    new_rj = (pi_j - thresh) / z[j]
+                else:
+                    new_rj = 0.0
+
+            delta = new_rj - rho[j]
+            if delta != 0.0:
+                rho[j] = new_rj
+                Grho += G[:, j] * delta
+                max_change = max(max_change, abs(delta))
+        #print(it, max_change)
+
+        if max_change < tol:
+            break
+
+    return rho
