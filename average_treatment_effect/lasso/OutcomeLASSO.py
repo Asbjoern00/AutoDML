fe20a063a69543536a2479b2e5ea04c332495e93
asbjoern00 <asbjoern00@gmail.com>
2025-12-20 10:29:04 +0100

    feat: OutcomeLASSO

---
+++ average_treatment_effect/lasso/OutcomeLASSO.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.linear_model import LassoCV
+class OutcomeLASSO:
+    def __init__(self, functional):
+        self.model = LassoCV(fit_intercept=True)
+        self.functional = functional
+
+    def fit(self, data):
+        X = np.concatenate([data.treatments.reshape(-1,1), data.covariates], axis = 1)
+        y = data.outcomes
+        self.model.fit(X, y)
+
+    def predict(self,data):
+        X = np.concatenate([data.treatments.reshape(-1,1), data.covariates], axis = 1)
+        return self.model.predict(X)
+
+    def get_residuals(self,data):
+        fitted = self.predict(data)
+        observed = data.outcomes
+        return fitted - observed
+
+    def get_functional(self, data):
+        return self.functional(data,self.predict)
+
+    def get_plugin_estimate(self,data):
+        return np.mean(self.get_functional(data))
+
+
+
+
+
+
+
