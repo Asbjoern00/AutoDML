7a53d86ce92fcece199ec042150936481aef2a9e
asbjoern00 <asbjoern00@gmail.com>
2025-12-21 10:02:38 +0100

    feat: cross-validate lasso penalty

--- average_treatment_effect/lasso/RieszLasso.py
+++ average_treatment_effect/lasso/RieszLasso.py
@@ -23,9 +23,33 @@ class RieszLasso:
         mb = self.functional(data, self.make_design_matrix)
         hatM = np.mean(mb, axis=0)
         hatG = 1 / xb.shape[0] * (xb.T @ xb)
-        #If rho is already fitted, warm state from rho
-        rho = md_lasso(hatG, hatM, penalty=penalty, rho_init=self.rho)
+        # If rho is already fitted, warm state from rho
+        rho = md_lasso(hatG, hatM, rL=penalty, rho_init=self.rho)
         self.rho = rho
+
+    def fit_cv(self,data,penalties = np.array([0.2,0.15,0.1,0.075,0.05]), n_folds = 5):
+        folds = data.split_into_folds(n_folds)
+        best_loss = np.inf
+        best_rho = None
+
+        for penalty in penalties:
+            cur_loss = 0
+            for i in range(n_folds):
+                test_data, train_data = data.get_fit_and_train_folds(folds, i)
+                self.fit(train_data, penalty)
+                riesz_loss = self.get_riesz_loss(test_data)
+                cur_loss += riesz_loss
+
+            if cur_loss < best_loss:
+                best_loss = cur_loss
+                best_rho = penalty
+
+        self.fit(data,best_rho)
+
+    def get_riesz_loss(self,data):
+        mb = self.functional(data, self.make_design_matrix)
+        loss = -2*mb@self.rho + self.get_riesz_representer(data)**2
+        return np.mean(loss)
 
     def get_riesz_representer(self, data):
         xb = self.make_design_matrix(data)
@@ -33,7 +57,7 @@ class RieszLasso:
 
 
 class PropensityLasso:
-    def __init__(self):
+    def __init__(self,_=None):
         self.model = LogisticRegressionCV(penalty="l1", fit_intercept=True, solver = "liblinear")
 
     def fit(self, data):
@@ -44,5 +68,5 @@ class PropensityLasso:
     def get_riesz_representer(self, data):
         covariates = data.covariates
         treatments = data.treatments
-        pi = self.model.predict_proba(covariates)[:,self.model.classes_ == 1]
+        pi = self.model.predict_proba(covariates)[:,self.model.classes_ == 1].reshape(treatments.shape[0],)
         return treatments/pi - (1-treatments)/(1-pi)
