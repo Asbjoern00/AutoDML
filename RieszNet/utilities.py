dbc35bfa44191f18299984d72230af3ef07b9281
asbjoern00 <asbjoern00@gmail.com>
2025-12-27 19:26:05 +0100

    feat : Construct general sequential neural net

---
+++ RieszNet/utilities.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+def make_sequential(in_dim,hidden_dim,out_dim,n_hidden,activation=nn.ELU):
+    layers = []
+    dims = [in_dim] + [hidden_dim] * n_hidden + [out_dim]
+
+    for d_in, d_out in zip(dims[:-1], dims[1:]):
+        layers.append(nn.Linear(d_in, d_out))
+        if d_out != out_dim:
+            layers.append(activation())
+
+    return nn.Sequential(*layers)
