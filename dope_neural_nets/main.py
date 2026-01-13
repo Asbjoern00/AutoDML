d017eed1d708b81f7883c52c25d8092281229e40
AsgerWP <lzt134@alumni.ku.dk>
2026-01-13 12:53:38 +0100

    feat: main file

--- dope_neural_nets/main.py
+++ dope_neural_nets/main.py
@@ -5,19 +5,28 @@ from dope_neural_nets.dataset import Dataset
 
 np.random.seed(1)
 
+truth = 2.121539888279284
+
 
 def run_experiment():
-   data = Dataset.simulate_dataset(1000, 10)
-   model_wrapper = ModelWrapper()
-   folds = data.split_into_folds(5)
-   estimate_components = []
-   for j in range(5):
-       fit_fold, train_folds = Dataset.get_fit_and_train_folds(folds, j)
-       model_wrapper.train_outcome_head(train_folds, train_shared_layers=True)
-       model_wrapper.train_riesz_head(train_folds, train_shared_layers=False)
-       estimate_components.append(model_wrapper.get_estimate_components(fit_fold))
-   estimate_components = torch.concat(estimate_components, dim=0)
-   estimate = torch.mean(estimate_components).item()
-   variance = torch.var(estimate_components).item()
-   return {'estimate': estimate, 'variance': variance}
-print(run_experiment())
+    data = Dataset.simulate_dataset(1000, 10)
+    model_wrapper = ModelWrapper()
+    folds = data.split_into_folds(5)
+    estimate_components = []
+    for j in range(5):
+        fit_fold, train_folds = Dataset.get_fit_and_train_folds(folds, j)
+        model_wrapper.train_outcome_head(train_folds, train_shared_layers=True)
+        model_wrapper.train_riesz_head(train_folds, train_shared_layers=False)
+        estimate_components.append(model_wrapper.get_estimate_components(fit_fold))
+    estimate_components = torch.concat(estimate_components, dim=0)
+    estimate = torch.mean(estimate_components).item()
+    variance = torch.var(estimate_components).item()
+    return {"estimate": estimate, "variance": variance}
+
+
+estimates = []
+for i in range(1000):
+    result = run_experiment()
+    estimates.append(result["estimate"])
+    mse = sum((est - truth) ** 2 for est in estimates) / len(estimates)
+    print(i, "RMSE:", mse**0.5)
