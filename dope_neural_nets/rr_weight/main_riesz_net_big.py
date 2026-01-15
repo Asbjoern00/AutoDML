b9f5117d2b5121b59ba033cd55777bcb07ba4093
AsgerWP <lzt134@alumni.ku.dk>
2026-01-15 12:02:48 +0100

    use new wrapper in main_riesz_net_big.py

--- dope_neural_nets/rr_weight/main_riesz_net_big.py
+++ dope_neural_nets/rr_weight/main_riesz_net_big.py
@@ -9,14 +9,13 @@ torch.manual_seed(42)
 truth = 2.121539888279284
 
 
-def run_experiment():
-    data = Dataset.simulate_dataset(1000, 10)
+def run_experiment(data):
     folds = data.split_into_folds(5)
     estimate_components = []
     for j in range(5):
         fit_fold, train_folds = Dataset.get_fit_and_train_folds(folds, j)
-        model_wrapper = ModelWrapper()
-        model_wrapper.train_as_riesz_net(train_folds, rr_w=10)
+        model_wrapper = ModelWrapper(in_=10, hidden_size=100, n_shared=2, n_not_shared=1)
+        model_wrapper.train_as_riesz_net(train_folds, tmle_w=0, rr_w=10, mse_w=1)
         estimate_components.append(model_wrapper.get_estimate_components(fit_fold))
     estimate_components = torch.concat(estimate_components, dim=0)
     estimate = torch.mean(estimate_components).item()
@@ -24,21 +23,24 @@ def run_experiment():
     return {
         "estimate": estimate,
         "variance": variance,
-        "lower": estimate - 1.96 * (variance / 1000) ** 0.5,
-        "upper": estimate + 1.96 * (variance / 1000) ** 0.5,
+        "lower": estimate - 1.96 * (variance / data.raw_data.shape[0]) ** 0.5,
+        "upper": estimate + 1.96 * (variance / data.raw_data.shape[0]) ** 0.5,
     }
 
+
 results = []
+truths = []
 for i in range(1000):
-    result = run_experiment()
+    data = Dataset.simulate_dataset(1000, 10)
+    result = run_experiment(data)
+    truths.append(truth)
     results.append(result)
-    residuals = [np.abs(result['estimate'] - truth) for result in results]
+    residuals = [np.abs(result["estimate"] - truth) for result, truth in zip(results, truths)]
     mse = sum(residual**2 for residual in residuals) / len(residuals)
-    mse_filtered = sum(residual**2 for residual in residuals if residual <= 1) / sum(1 for residual in residuals if residual <= 1)
-    coverage = sum(result["lower"] <= truth <= result["upper"] for result in results) / len(results)
-    print(i, "Estimate:", result["estimate"], "RMSE:", mse**0.5, 'Coverage:', coverage, 'Filtered RMSE:', mse_filtered ** 0.5)
+    coverage = sum(result["lower"] <= truth <= result["upper"] for result, truth in zip(results, truths)) / len(results)
+    print(i, truth, "Estimate:", result["estimate"], "RMSE:", mse**0.5, "Coverage:", coverage)
 
 import pandas as pd
 
 estimates = pd.DataFrame(results)
-estimates.to_csv("dope_neural_nets/rr_weight/rr_10.csv", index=False)
+estimates.to_csv("dope_neural_nets/rr_weight/rrw_10.csv", index=False)
