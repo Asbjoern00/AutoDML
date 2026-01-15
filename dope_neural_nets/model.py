ed9ba900bae577aa165c64747fe1675f0362fd74
AsgerWP <lzt134@alumni.ku.dk>
2026-01-15 11:30:52 +0100

    feat: remove prints

--- dope_neural_nets/model.py
+++ dope_neural_nets/model.py
@@ -30,11 +30,20 @@ class ModelWrapper:
         train_data, val_data = data.test_train_split(0.8)
         train_treated, train_control = train_data.get_counterfactual_datasets()
         val_treated, val_control = val_data.get_counterfactual_datasets()
+        loader = DataLoader(
+            TensorDataset(
+                train_data.net_input, train_treated.net_input, train_control.net_input, train_data.outcomes_tensor
+            ),
+            batch_size=32,
+            shuffle=True,
+        )
+
         best = 1e6
         patience = 20
         if isinstance(lr, list):
             for lr_ in lr:
                 best = self._train_as_riesz_net(
+                    loader,
                     best,
                     lr_,
                     mse_w,
@@ -44,9 +53,6 @@ class ModelWrapper:
                     rr_w,
                     tmle_criterion,
                     tmle_w,
-                    train_control,
-                    train_data,
-                    train_treated,
                     val_control,
                     val_data,
                     val_treated,
@@ -54,6 +60,7 @@ class ModelWrapper:
                 )
         else:
             self._train_as_riesz_net(
+                loader,
                 best,
                 lr,
                 mse_w,
@@ -63,9 +70,6 @@ class ModelWrapper:
                 rr_w,
                 tmle_criterion,
                 tmle_w,
-                train_control,
-                train_data,
-                train_treated,
                 val_control,
                 val_data,
                 val_treated,
@@ -74,6 +78,7 @@ class ModelWrapper:
 
     def _train_as_riesz_net(
         self,
+        loader,
         best,
         lr,
         mse_w,
@@ -83,9 +88,6 @@ class ModelWrapper:
         rr_w,
         tmle_criterion,
         tmle_w,
-        train_control,
-        train_data,
-        train_treated,
         val_control,
         val_data,
         val_treated,
@@ -95,22 +97,22 @@ class ModelWrapper:
         counter = 0
         best_state = copy.deepcopy(self.model.state_dict())
         for epoch in range(1000):
-            optimizer.zero_grad()
-            actual_riesz = self.model.predict_riesz(train_data.net_input)
-            treated_riesz = self.model.predict_riesz(train_treated.net_input)
-            control_riesz = self.model.predict_riesz(train_control.net_input)
-            riesz_loss = riesz_criterion(actual_riesz, treated_riesz, control_riesz)
-            base_predictions = self.model.predict_without_correction(train_data.net_input)
-            outcome_loss = outcome_criterion(base_predictions, train_data.outcomes_tensor)
-            tmle_w_loss = tmle_criterion(self.model.predict_outcome(train_data.net_input), train_data.outcomes_tensor)
-            l2_penalty = 0.0
-            for name, param in self.model.named_parameters():
-                if name != "epsilon":
-                    l2_penalty += torch.sum(param**2)
-            loss = riesz_loss * rr_w + outcome_loss * mse_w + tmle_w_loss * tmle_w + l2_penalty * wd
-            loss.backward()
-            optimizer.step()
-
+            for x, xt, xc, y in loader:
+                optimizer.zero_grad()
+                actual_riesz = self.model.predict_riesz(x)
+                treated_riesz = self.model.predict_riesz(xt)
+                control_riesz = self.model.predict_riesz(xc)
+                riesz_loss = riesz_criterion(actual_riesz, treated_riesz, control_riesz)
+                base_predictions = self.model.predict_without_correction(x)
+                outcome_loss = outcome_criterion(base_predictions, y)
+                tmle_w_loss = tmle_criterion(self.model.predict_outcome(x), y)
+                l2_penalty = 0.0
+                for name, param in self.model.named_parameters():
+                    if name != "epsilon":
+                        l2_penalty += torch.sum(param**2)
+                loss = riesz_loss * rr_w + outcome_loss * mse_w + tmle_w_loss * tmle_w + l2_penalty * wd
+                loss.backward()
+                optimizer.step()
             with torch.no_grad():
                 actual_riesz = self.model.predict_riesz(val_data.net_input)
                 treated_riesz = self.model.predict_riesz(val_treated.net_input)
@@ -127,7 +129,6 @@ class ModelWrapper:
             else:
                 counter += 1
             if counter >= patience:
-                print(epoch, outcome_loss)
                 break
         print(outcome_loss, riesz_loss,test_loss)
         self.model.load_state_dict(best_state)
@@ -179,7 +180,6 @@ class ModelWrapper:
             else:
                 counter += 1
             if counter >= patience:
-                print("outcome", best, epoch)
                 break
         self.model.load_state_dict(best_state)
         return best
@@ -244,7 +244,6 @@ class ModelWrapper:
             else:
                 counter += 1
             if counter >= patience:
-                print("riesz", best, epoch)
                 break
         self.model.load_state_dict(best_state)
         return best
