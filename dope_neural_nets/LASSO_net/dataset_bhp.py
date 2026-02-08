144ce6ae1490f5c091f8c8719c972e6acbcf0791
AsgerWP <lzt134@alumni.ku.dk>
2026-02-08 23:38:27 +0100

    fix truth

--- dope_neural_nets/LASSO_net/dataset_bhp.py
+++ dope_neural_nets/LASSO_net/dataset_bhp.py
@@ -5,7 +5,7 @@ import torch
 
 class Dataset:
     def __init__(
-        self, raw_data: np.ndarray, outcome_column: int, treatment_column: int, covariate_columns: list = None
+        self, raw_data: np.ndarray, outcome_column: int, treatment_column: int, covariate_columns: list = None, truth=None
     ):
         self.raw_data = raw_data
         self.outcome_column = outcome_column
@@ -13,19 +13,22 @@ class Dataset:
         if not covariate_columns:
             covariate_columns = [i for i in range(raw_data.shape[1]) if i not in [outcome_column, treatment_column]]
         self.covariate_columns = covariate_columns
+        self.truth = truth
 
     @classmethod
     def from_csv(cls, path):
         data = np.loadtxt(path, skiprows=1, delimiter=",")
+        truth = np.mean(data[:, 0])
         data = data[:, 1:]
         return cls(
             raw_data=data,
             treatment_column=1,
             outcome_column=0,
+            truth=truth
         )
 
     def get_truth(self):
-        return -0.6
+        return self.truth
 
     @classmethod
     def load_chernozhukov_replication(cls, index):
