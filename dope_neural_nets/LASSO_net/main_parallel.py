18ef161bb2020bfdaa4da7beb3c5135b881a2721
AsgerWP <lzt134@alumni.ku.dk>
2026-02-11 16:45:16 +0100

    use group lasso for ihdp

--- dope_neural_nets/LASSO_net/main_parallel.py
+++ dope_neural_nets/LASSO_net/main_parallel.py
@@ -12,20 +12,8 @@ torch.set_num_interop_threads(1)
 
 def _run_iteration(data):
     estimate_components = []
-    train, test = data.test_train_split(0.8)
-    penalty = 0
-    best = 1e6
-    penalties = [0, 1e-3, 1e-2, 1e-1, 1]
-    for pen in penalties:
-        model_wrapper_ = ModelWrapper(in_=25, hidden_size=100, n_shared=3, n_not_shared=2)
-        model_wrapper_.train_outcome_head(train, train_shared_layers=True, lr=1e-3, l1_penalty=pen)
-        res = model_wrapper_._get_mse_loss(test.net_input, test.outcomes_tensor).item()
-        # print(pen, res)
-        if res < best:
-            best = res
-            penalty = pen
     model_wrapper = ModelWrapper(in_=25, hidden_size=100, n_shared=3, n_not_shared=2)
-    model_wrapper.train_outcome_head(data, train_shared_layers=True, lr=1e-3, l1_penalty=penalty)
+    model_wrapper.train_outcome_head(data, train_shared_layers=True, lr=1e-3, l1_penalty=1)
     model_wrapper.train_riesz_head(data, train_shared_layers=False, lr=1e-3)
     estimate_components.append(model_wrapper.get_estimate_components(data))
     estimate_components = torch.concat(estimate_components, dim=0)
@@ -48,40 +36,41 @@ def run_experiment(indices):
         data = Dataset.load_chernozhukov_replication(i + 1)
         result = _run_iteration(data)
         results.append(result)
-        residuals = [result["estimate"] - result["truth"] for result in results]
-        MSE = sum(residual**2 for residual in residuals) / len(residuals)
-        mae = sum(abs(residual) for residual in residuals) / len(residuals)
-        bias = sum(residuals) / len(residuals)
-        coverage = sum(result["lower"] <= result["truth"] <= result["upper"] for result in results) / len(results)
-        print(
-            i,
-            data.get_truth(),
-            "Estimate:",
-            result["estimate"],
-            "RMSE:",
-            MSE**0.5,
-            "Bias:",
-            bias,
-            "MAE",
-            mae,
-            "Coverage:",
-            coverage,
-        )
+        _print_diagnostics(results)
+        return results
 
-    return results
-
+def _print_diagnostics(results):
+    residuals = [result["estimate"] - result["truth"] for result in results]
+    MSE = sum(residual**2 for residual in residuals) / len(residuals)
+    mae = sum(abs(residual) for residual in residuals) / len(residuals)
+    bias = sum(residuals) / len(residuals)
+    mean_est = sum(result["estimate"] for result in results) / len(results)
+    variance = sum((result["estimate"] - mean_est) ** 2 for result in results) / len(results)
+    coverage = sum(result["lower"] <= result["truth"] <= result["upper"] for result in results) / len(results)
+    print(
+        "RMSE:",
+        MSE**0.5,
+        "Bias:",
+        bias,
+        "MAE",
+        mae,
+        "Coverage:",
+        coverage,
+    )
 
 if __name__ == "__main__":
     indices = [i for i in range(1000)]
-    chunk_size = 167
+    chunk_size = 1
     chunks = [indices[i : i + chunk_size] for i in range(0, len(indices), chunk_size)]
     mp.set_start_method("spawn", force=True)
-    n_proc = 6
-
-    with mp.Pool(processes=n_proc) as p:
-        results = p.map(run_experiment, chunks)
+    n_proc = 5
     result = []
-    for res in results:
-        result += res
+    for i in range(200):
+        with mp.Pool(processes=n_proc) as p:
+            results = p.map(run_experiment, chunks[i * 5 : (i + 1) * 5])
+        for res in results:
+            result += res
+        print("Total Iterations", len(result))
+        _print_diagnostics(result)
     df = pd.DataFrame(result)
-    df.to_csv("dope_neural_nets/LASSO_net/lasso_net.csv", index=False)
+    df.to_csv("dope_neural_nets/LASSO_net/lasso_net_ihdp.csv", index=False)
