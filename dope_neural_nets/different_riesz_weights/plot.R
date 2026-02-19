cc9a215864bd7629e0e06985f5830eb97eb004d0
AsgerWP <lzt134@alumni.ku.dk>
2026-02-19 14:43:16 +0100

    plot

--- dope_neural_nets/different_riesz_weights/plot.R
+++ dope_neural_nets/different_riesz_weights/plot.R
@@ -5,11 +5,15 @@ data05 <- read_csv("dope_neural_nets/different_riesz_weights/tmle05_full_x.csv")
   mutate(type = 'TMLE weight 0.5, Outcome MSE weight 0.5', index=1:50)
 data1 <- read_csv("dope_neural_nets/different_riesz_weights/tmle1_full_x.csv") %>%
   mutate(type = 'TMLE weight 1, Outcome MSE weight 0', index=1:50)
+data_outcome <- read_csv("dope_neural_nets/outcome_informed_ihdp/outcome_informed_ihdp_full.csv") %>%
+  mutate(type = 'Outcome informed net', index=1:1000) 
+
+
 
 data = rbind(data0[1:30,],data05[1:30,], data1[1:30,])
 ggplot(data) +
-  geom_line(aes(x=index, y=estimate-truth))+
-  facet_wrap(~type)+
+  geom_line(aes(x=index, y=estimate-truth, color=type), size=0.3, linetype='dashed')+
+  geom_point(aes(x=index, y=estimate-truth, color=type), size=3)+
   xlab('index')+
   ylab('estimation error')+
   theme_classic()+
