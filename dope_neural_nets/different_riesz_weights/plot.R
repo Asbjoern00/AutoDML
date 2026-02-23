67e71a8d4d350c44fce6333c6fee5bca49865f27
AsgerWP <lzt134@alumni.ku.dk>
2026-02-23 12:37:38 +0100

    save

--- dope_neural_nets/different_riesz_weights/plot.R
+++ dope_neural_nets/different_riesz_weights/plot.R
@@ -2,16 +2,32 @@ library(tidyverse)
 data0 <- read_csv("dope_neural_nets/different_riesz_weights/tmle0.csv") %>%
   mutate(type = 'TMLE weight 0, Outcome MSE weight 1', index=1:1000, tmle_w = 0)
 data05 <- read_csv("dope_neural_nets/different_riesz_weights/tmle0.5.csv") %>%
-  mutate(type = 'TMLE weight 0, Outcome MSE weight 1', index=1:1000, tmle_w = 0.5)
+  mutate(type = 'TMLE weight 0.5, Outcome MSE weight 1.5', index=1:1000, tmle_w = 0.5)
 data1 <- read_csv("dope_neural_nets/different_riesz_weights/tmle1.csv") %>%
   mutate(type = 'TMLE weight 0.5, Outcome MSE weight 0.5', index=1:1000, tmle_w = 1)
 data15 <- read_csv("dope_neural_nets/different_riesz_weights/tmle1.5.csv") %>%
-  mutate(type = 'TMLE weight 0.5, Outcome MSE weight 0.5', index=1:1000, tmle_w = 1.5)
+  mutate(type = 'TMLE weight 1.5, Outcome MSE weight 0.5', index=1:1000, tmle_w = 1.5)
 data2 <- read_csv("dope_neural_nets/different_riesz_weights/tmle2.csv") %>%
-  mutate(type = 'TMLE weight 1, Outcome MSE weight 0', index=1:1000, tmle_w = 2)
+  mutate(type = 'TMLE weight 2, Outcome MSE weight 0', index=1:1000, tmle_w = 2)
+
+
+
 
 data = rbind(data0, data05, data1,data15, data2)
-data = data[data$index <= 1000, ]
+
+d = data %>%
+  mutate(residual = (estimate - truth)) %>%
+  select(index, tmle_w, residual) %>%
+  pivot_wider(
+    names_from = tmle_w,
+    values_from = residual
+  ) %>%
+  select(-index)
+
+corrplot::corrplot(cor(d), method = 'number')
+
+
+
 data = data %>%
   mutate(residual_sq = (estimate-truth)^2) %>%
   group_by(tmle_w) %>%
@@ -20,14 +36,12 @@ data = data %>%
 
 ggplot(data) +
   geom_point(aes(x=tmle_w, y = rmse)) + 
-  geom_errorbar(aes(x=tmle_w, ymin=l, ymax=u))+
+  geom_errorbar(aes(x=tmle_w, ymin=l, ymax=u), width=.1)+
   geom_line(aes(x=tmle_w, y = rmse), linetype='dashed') + 
   theme_classic()+
-  ylim(0,0.3)
-
+  ylim(0,0.2)+
+  xlab('Weight on TMLE component')+
+  ylab('RMSE')+
+  ggtitle('RMSE with constant total weight on TMLE and MSE loss components')+
+  theme(plot.title = element_text(hjust = 0.5))
 data
-
-
-  
-
-
