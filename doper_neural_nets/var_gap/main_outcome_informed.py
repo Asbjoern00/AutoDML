a62c985721aff63994f2fa2548e046097e95a429
AsgerWP <lzt134@alumni.ku.dk>
2026-03-11 17:08:13 +0100

    save

--- doper_neural_nets/var_gap/main_outcome_informed.py
+++ doper_neural_nets/var_gap/main_outcome_informed.py
@@ -3,15 +3,19 @@ import numpy as np
 from doper_neural_nets.var_gap.model import ModelWrapper
 from doper_neural_nets.var_gap.dataset import Dataset
 
-n = 10000
+n = 100
 p = 2
 beta = 2
+
+import pandas as pd
+output_file = f"doper_neural_nets/var_gap/outcome_informed_{beta}.csv"
+
 
 def run_experiment(data):
     fit, est = data.test_train_split(0.5)
     model_wrapper = ModelWrapper(in_=p, hidden_size=100, n_shared=3, n_not_shared=2)
-    model_wrapper.train_outcome_head(fit, lr=1e-3, train_shared_layers=True, epochs=1000, wd=1e-3, l1=0.1, batch_size=64)
-    model_wrapper.train_riesz_head(fit, lr=1e-3, train_shared_layers=False, epochs=1000, wd=1e-3, batch_size=1000)
+    model_wrapper.train_outcome_head(fit, lr=1e-3, train_shared_layers=True, epochs=3, wd=1e-3, l1=0.1, batch_size=64)
+    model_wrapper.train_riesz_head(fit, lr=1e-3, train_shared_layers=False, epochs=3, wd=1e-3, batch_size=1000)
     estimate_components = model_wrapper.get_estimate_components(est)
     estimate = torch.mean(estimate_components).item()
     variance = torch.var(estimate_components).item()
@@ -28,17 +32,14 @@ results = []
 for i in range(1000):
     np.random.seed(i)
     torch.manual_seed(i)
-    data = Dataset.simulate_dataset(n,p)
+    data = Dataset.simulate_dataset(n, p)
     result = run_experiment(data)
     results.append(result)
     residuals = [np.abs(result["estimate"] - result["truth"]) for result in results]
     MAE = sum(residual for residual in residuals) / len(residuals)
     MSE = sum(residual**2 for residual in residuals) / len(residuals)
     coverage = sum(result["lower"] <= result["truth"] <= result["upper"] for result in results) / len(results)
-    print(i, "nMSE", MSE*n/2)
+    print(i, "nMSE", MSE * n / 2)
 
-
-import pandas as pd
-
-estimates = pd.DataFrame(results)
-estimates.to_csv(f"doper_neural_nets/var_gap/outcome_informed_{beta}.csv", index=False)
+    df = pd.DataFrame([result])
+    df.to_csv(output_file, mode="a", header=not pd.io.common.file_exists(output_file), index=False)
--- doper_neural_nets/var_gap/main_separate_nets.py
+++ doper_neural_nets/var_gap/main_separate_nets.py
@@ -7,10 +7,15 @@ n = 10000
 p = 2
 beta = 2
 
+import pandas as pd
+
+output_file = f"doper_neural_nets/var_gap/separate_nets_{beta}.csv"
+
+
 def run_experiment(data):
     fit, est = data.test_train_split(0.5)
     model_wrapper = ModelWrapper(in_=p, hidden_size=100, n_shared=3, n_not_shared=2, type_="separate_nets")
-    model_wrapper.train_outcome_head(fit, lr=1e-3,train_shared_layers=True, epochs=1000, wd=1e-3, batch_size=64)
+    model_wrapper.train_outcome_head(fit, lr=1e-3, train_shared_layers=True, epochs=1000, wd=1e-3, batch_size=64)
     model_wrapper.train_riesz_head(fit, lr=1e-3, train_shared_layers=True, epochs=1000, wd=1e-3, batch_size=1000)
     estimate_components = model_wrapper.get_estimate_components(est)
     estimate = torch.mean(estimate_components).item()
@@ -37,8 +42,5 @@ for i in range(1000):
     coverage = sum(result["lower"] <= result["truth"] <= result["upper"] for result in results) / len(results)
     print(i, "nMSE", MSE * n / 2)
 
-
-import pandas as pd
-
-estimates = pd.DataFrame(results)
-estimates.to_csv(f"doper_neural_nets/var_gap/separate_nets_{beta}.csv", index=False)
+    df = pd.DataFrame([result])
+    df.to_csv(output_file, mode="a", header=not pd.io.common.file_exists(output_file), index=False)
